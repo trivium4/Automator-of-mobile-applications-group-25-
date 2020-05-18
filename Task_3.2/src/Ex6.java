38dfd5ae6569803f9355bc942da4868169889c55
Saya <trivium4@live.com>
2020-05-18 20:48:44 +0400

    Task_3.2

---
+++ Task_3.2/src/Ex6.java
@@ -0,0 +1,22 @@
+import io.appium.java_client.AppiumDriver;
+import lib.CoreTestCase;
+import lib.ui.ArticlePageObject;
+import lib.ui.SearchPageObject;
+import org.junit.Test;
+
+public class Ex6 extends CoreTestCase {
+
+    @Test
+    public void testTitleInArticlePresence()
+    {
+        SearchPageObject SearchPageObject = new SearchPageObject(driver);
+
+        SearchPageObject.initSearchInput();
+        SearchPageObject.typeSearchLine("Java");
+        SearchPageObject.clickByArticleWithSubstring("Object-oriented programming language");
+
+        ArticlePageObject ArticlePageObject = new ArticlePageObject(driver);
+        ArticlePageObject.waitForTitleElement();
+    }
+
+}
